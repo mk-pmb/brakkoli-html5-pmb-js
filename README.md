a412c0b62923da31b1670fb15c6c29fe13c5203c
mk-pmb <mk-pmb@users.noreply.github.com>
2017-04-20 02:57:35 +0200

    basic page

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+brakkoli-html5-pmb
+==================
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+My favorite HTML5 fragments for brakkoli.
+<!--/#echo -->
+
+
+&nbsp;
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ basic_page.js
@@ -0,0 +1,26 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false */
+/* -*- tab-width: 2 -*- */
+/*global define: true, module: true, require: true */
+((typeof define === 'function') && define.amd ? define : function (factory) {
+  'use strict';
+  var m = ((typeof module === 'object') && module), e = (m && m.exports);
+  if (e) { m.exports = (factory(require, e, m) || m.exports); }
+})(function () {
+  'use strict';
+
+  return function basicWebPage(opts) {
+    var pg = [ '', [ '!DOCTYPE html>' ] ],
+      indent = (opts.indent && { indentTags: opts.indent });
+    pg.head = [ 'head', indent,
+      { metaHttpEquiv: { Content_Type: 'text/html; charset=UTF-8' } },
+      (opts.title && [ 'title', opts.title ]),
+      ].filter(Boolean);
+    pg.head.title = opts.title;
+    pg.body = [ 'body', indent,
+      ].filter(Boolean);
+    pg.push([ 'html', pg.head, pg.body ]);
+    if (indent) { pg.push('\n'); }
+    return pg;
+  };
+
+});
